0bd415747b2959c150f0053fe81309465a66cd25
Nick Spinale <nick@nickspinale.com>
2025-11-21 02:40:22 -0800

    Update rust-sel4
    Signed-off-by: Nick Spinale <nick@nickspinale.com>

--- crates/assistant/core/src/lib.rs
+++ crates/assistant/core/src/lib.rs
@@ -11,7 +11,9 @@ extern crate alloc;
 use alloc::vec;
 use alloc::vec::Vec;
 
-use ab_glyph::{point, Font, FontRef, Glyph, Point, PxScale, ScaleFont};
+use ab_glyph::{Font, FontRef, Glyph, Point, PxScale, ScaleFont, point};
+
+#[allow(unused_imports)]
 use num_traits::Float;
 
 pub struct Draft {
--- update.sh
+++ update.sh
@@ -19,8 +19,7 @@ cp -r $external_demo_dir crates
 find crates -name Cargo.nix -delete
 mv crates/pds/* crates/
 rmdir crates/pds
-mv crates/*.system.template .
-mv crates/generate_system_description.py .
+rm crates/meta.py
 
 subst='s,path = "\(../\)*../../../../\([^"]*\)",git = "https://github.com/seL4/rust-sel4",g' \
 
